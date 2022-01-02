6503e6b7ca9a2042f8d7c093294254c8b630e516
javikalsan <jcalzado@alea-soluciones.com>
2022-01-02 15:16:50 +0100

    Update script using new data sources

--- run.sh
+++ run.sh
@@ -1,50 +1,36 @@
 #!/bin/bash
 
-SOURCE_URL="http://www.navarra.es/appsext/DescargarFichero/default.aspx?codigoAcceso=OpenData&fichero=coronavirus\datos_coronavirus_julio_2020.xls"
-XLS_SOURCE_DATA="default.aspx?codigoAcceso=OpenData&fichero=coronavirus\datos_coronavirus_julio_2020.xls"
-TEMP_DATA_FILENAME=data__$(date -I).csv
+SOURCE_URL="https://datosabiertos.navarra.es/datastore/dump/1b600bb6-a6a7-43c7-a551-23ebd5ade374?format=csv&bom=True"
 DATA_FILENAME=data_$(date -I).csv
 
 ####################
 # DATA ACQUISITION #
 ####################
 
-# Download opendata official source
-wget $SOURCE_URL &&
+# Download opendata official CSV source
+wget $SOURCE_URL -O $DATA_FILENAME
 
 
 #######################
 # DATA TRANSFORMATION #
 #######################
 
-# Use gnumeric ssconvert tool to convert propietary xls format to csv
-ssconvert "$XLS_SOURCE_DATA" "$TEMP_DATA_FILENAME" &&
+## Get columns 2, 3 and 7
+gawk -i inplace -F"," '{print $2",",$3",",$7}' $DATA_FILENAME
 
-# Remove first five lines
-sed -i '1,5d' "$TEMP_DATA_FILENAME" &&
-
-# Get columns 1, 3 and 12
-cut -d ',' -f1,3,12 < "$TEMP_DATA_FILENAME" > "$DATA_FILENAME" &&
-
-# Remove temporary data file
-rm "$TEMP_DATA_FILENAME" &&
-
-# Remove first line
+## Remove first line
 sed -i '1d' "$DATA_FILENAME" &&
 
-# Add custom columns at the first line
+## Add custom columns at the first line
 sed -i "1s/^/$(cat columns)\n/" "$DATA_FILENAME" &&
 
-# Remove double quotes
+## Remove double quotes
 sed -i 's/"//g' "$DATA_FILENAME" &&
-
-# Remove original source data
-rm *.xls &&
 
 
 #############
 # DATA LOAD #
 #############
 
-# Generate plot
+## Generate plot
 python plot.py
